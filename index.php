fbc8cd47b764d01472918cd4cd7c56ef9d862761
maximiliano1401 <alu.2411155@itesrenedescartes.edu.mx>
2025-10-28 12:15:01 -0600

    Añadir sección "Conoce sobre CodeLab" con contenido y estilos actualizados

--- index.php
+++ index.php
@@ -12,9 +12,33 @@
 
     <!-- Sección: Conoce sobre CodeLab -->
     <section class="seccion-conoce">
+        <h2 class="titulo-seccion">Conoce sobre CodeLab</h2>
         <div class="contenedor-conoce">
-            <div class="tarjeta-texto"></div>
-            <div class="imagen-persona"></div>
+            <div class="tarjeta-texto">
+                <div class="etiqueta-tarjeta">
+                    <span>Lorem Ipsum</span>
+                    <span>•</span>
+                    <span>---</span>
+                </div>
+                <p class="texto-principal">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                </p>
+                <div class="botones-navegacion">
+                    <button class="btn-nav btn-nav-prev" aria-label="Anterior">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 16L6 10L12 4" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                    <button class="btn-nav btn-nav-next" aria-label="Siguiente">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8 4L14 10L8 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="imagen-persona">
+                <img src="assets/img/logo-registro-login.png" alt="CodeLab">
+            </div>
         </div>
     </section>
 
